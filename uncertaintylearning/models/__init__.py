9aeda0ce623795c8889f143093acd69b8fc9e899
Moksh Jain <mokshjn00@gmail.com>
2021-01-26 17:36:18 +0530

    improve deup training

--- uncertaintylearning/models/__init__.py
+++ uncertaintylearning/models/__init__.py
@@ -2,3 +2,4 @@ from .epistemic_predictor import EpistemicPredictor
 from .mcdropout import MCDropout
 from .evidential_regression import EvidentialRegression
 from .ensemble import Ensemble
+from .deup import DEUP
