1c7f85cc68cd6c152e4fad20f5ad293b4048c18e
Moksh Jain <mokshjn00@gmail.com>
2021-01-28 00:23:10 +0530

    add ood detection test

--- bin/ood_exps.py
+++ bin/ood_exps.py
@@ -8,8 +8,31 @@ from uncertaintylearning.models import DEUP
 import torch.optim as optim
 import torchvision 
 import torchvision.transforms as transforms
+from sklearn.metrics import roc_auc_score
 
 device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def test_ood(self, model, iid_loader, ood_loader):
+    scores = []
+    labels = []
+    
+    for inp, _ in iid_loader:
+        with torch.no_grad():
+            _, score = model.get_prediction_with_uncertainty(inp)
+        
+        scores.extend([score.cpu().numpy().tolist()])
+        labels.extend([0 for _ in range(inp.size(0))])
+    
+    for inp, _ in ood_loader:
+        with torch.no_grad():
+            _, score = model.get_prediction_with_uncertainty(inp)
+        
+        scores.extend([score.cpu().numpy().tolist()])
+        labels.extend([1 for _ in range(inp.size(0))])
+    
+    roc_auc = roc_auc_score(y_true=labels, y_score=scores)
+
+    print("OOD ROC AUC: {}".format(roc_auc))
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
@@ -25,18 +48,19 @@ iid_testset = torchvision.datasets.CIFAR10(root='/network/tmp1/moksh.jain/data',
 iid_testloader = torch.utils.data.DataLoader(iid_testset, batch_size=128,
                                          shuffle=False, num_workers=2)
 
-oodset = torchvision.datasets.SVHN(root='/network/tmp1/moksh.jain/data', split='test',
-                                         download=True, transform=transform)
-oodloader = torch.utils.data.DataLoader(oodset, batch_size=64,
-                                         shuffle=False, num_workers=2)
-
-# testset = torchvision.datasets.CIFAR100(root='/network/tmp1/moksh.jain/data', train=False,
-#                                        download=True, transform=transform)
-# testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+# oodset = torchvision.datasets.SVHN(root='/network/tmp1/moksh.jain/data', split='test',
+#                                          download=True, transform=transform)
+# oodloader = torch.utils.data.DataLoader(oodset, batch_size=64,
 #                                          shuffle=False, num_workers=2)
 
+testset = torchvision.datasets.CIFAR100(root='/network/tmp1/moksh.jain/data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=128,
+                                         shuffle=False, num_workers=2)
+
+# Train Density estimator on train set
 density_estimator = MAFMOGDensityEstimator(n_components=10, hidden_size=1024, batch_size=256, n_blocks=5, lr=1e-4, use_log_density=True, use_density_scaling=True)
-# density_estimator.fit(trainset, device)
+density_estimator.fit(trainset, device)
 
 networks = {
             'a_predictor': create_network(1, 1, 32, 'relu', True),
@@ -46,7 +70,6 @@ networks = {
 
 optimizers = {'a_optimizer': create_optimizer(networks['a_predictor'], 1e-2),
               'e_optimizer': create_optimizer(networks['e_predictor'], 3e-3),
-              # 'f_optimizer': create_optimizer(networks['f_predictor'], 1e-3)
               'f_optimizer': optim.Adam(networks['f_predictor'].parameters(), 1e-3)
               }
 
@@ -69,7 +92,5 @@ model = DEUP(data=data,
 model = model.to(device)
 
 epochs = 100
-
 new_losses = model.fit(epochs=epochs, val_loader=iid_testloader)
-for key in 'afe':
-    losses[key].extend(new_losses[key])
+model.fit_ood(epochs=epochs)
